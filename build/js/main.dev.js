34c3abf8f403102d0c1cfdeb9075e40cb5e66d6f
szatolokin <staszatolokin28@gmail.com>
2020-08-22 18:23:33 +0300

    test

---
+++ build/js/main.dev.js
@@ -0,0 +1,74 @@
+"use strict";
+
+var $document = $(document),
+    $wrapper = $(".wrapper");
+$document.ready(function () {
+  init();
+  docMouseClick();
+  topMenu();
+});
+
+function init() {
+  console.log("start");
+  $(".pets").css("background-color", "lightblue");
+}
+
+function docMouseClick() {
+  var clickCount = 0;
+  $(".pets").click(function (e) {
+    console.clear();
+    console.log(".pets");
+    console.log("page:", e.pageX, e.pageY);
+    console.log("screen:", e.screenX, e.screenY);
+    console.log("offset:", e.offsetX, e.offsetY);
+    console.log("client:", e.clientX, e.clientY);
+
+    if (clickCount++ % 3 === 0) {
+      var message = "";
+      message += "pets\n";
+      message += "page: ".concat(e.pageX, " ").concat(e.pageY, "\n");
+      message += "screen: ".concat(e.screenX, " ").concat(e.screenY, "\n");
+      message += "offset: ".concat(e.offsetX, " ").concat(e.offsetY, "\n");
+      message += "client: ".concat(e.clientX, " ").concat(e.clientY, "\n");
+      alert(message);
+    }
+
+    e.stopPropagation();
+  });
+  $document.click(function (e) {
+    console.clear();
+    console.log("#document");
+    console.log("page:", e.pageX, e.pageY);
+    console.log("screen:", e.screenX, e.screenY);
+    console.log("offset:", e.offsetX, e.offsetY);
+    console.log("client:", e.clientX, e.clientY);
+
+    if (clickCount++ % 3 === 0) {
+      var message = "";
+      message += "#document\n";
+      message += "page: ".concat(e.pageX, " ").concat(e.pageY, "\n");
+      message += "screen: ".concat(e.screenX, " ").concat(e.screenY, "\n");
+      message += "offset: ".concat(e.offsetX, " ").concat(e.offsetY, "\n");
+      message += "client: ".concat(e.clientX, " ").concat(e.clientY, "\n");
+      alert(message);
+    }
+  });
+}
+
+function topMenu() {
+  var scrollTop = $document.scrollTop();
+  $document.scroll(function () {
+    console.clear();
+    var newScrollTop = $document.scrollTop();
+
+    if (newScrollTop >= scrollTop) {
+      if (!$wrapper.hasClass("active")) {
+        $wrapper.addClass("active");
+      }
+    } else {
+      $wrapper.removeClass("active");
+    }
+
+    scrollTop = newScrollTop;
+  });
+}
---
+++ main.js
@@ -0,0 +1,86 @@
+"use strict";
+
+const 
+  $document = $(document),
+  $wrapper = $(".wrapper");
+
+$document.ready(function () {
+  init();
+
+  docMouseClick();
+
+  topMenu();
+});
+
+function init() {
+  console.log("start");
+  
+  $(".pets").css("background-color", "lightblue");
+}
+
+function docMouseClick() {
+  let clickCount = 0;
+
+  $(".pets").click(e => {
+    console.clear();
+
+    console.log(".pets");
+    console.log("page:", e.pageX, e.pageY);
+    console.log("screen:", e.screenX, e.screenY);
+    console.log("offset:", e.offsetX, e.offsetY);
+    console.log("client:", e.clientX, e.clientY);
+
+    if ((clickCount++ % 3) === 0) {
+      let message = "";
+      message += "pets\n";
+      message += `page: ${e.pageX} ${e.pageY}\n`;
+      message += `screen: ${e.screenX} ${e.screenY}\n`;
+      message += `offset: ${e.offsetX} ${e.offsetY}\n`;
+      message += `client: ${e.clientX} ${e.clientY}\n`;
+
+      alert(message);
+    }
+
+    e.stopPropagation();
+  });
+
+  $document.click(e => {
+    console.clear();
+
+    console.log("#document");
+    console.log("page:", e.pageX, e.pageY);
+    console.log("screen:", e.screenX, e.screenY);
+    console.log("offset:", e.offsetX, e.offsetY);
+    console.log("client:", e.clientX, e.clientY);
+
+    if ((clickCount++ % 3) === 0) {
+      let message = "";
+      message += "#document\n";
+      message += `page: ${e.pageX} ${e.pageY}\n`;
+      message += `screen: ${e.screenX} ${e.screenY}\n`;
+      message += `offset: ${e.offsetX} ${e.offsetY}\n`;
+      message += `client: ${e.clientX} ${e.clientY}\n`;
+
+      alert(message);
+    }
+  });
+}
+
+function topMenu() {
+  let scrollTop = $document.scrollTop();
+
+  $document.scroll(() => {
+    console.clear();
+
+    let newScrollTop = $document.scrollTop();
+    if (newScrollTop >= scrollTop) {
+      if (!$wrapper.hasClass("active")) {
+        $wrapper.addClass("active");
+      }
+    } else {
+      $wrapper.removeClass("active");
+    }
+
+    scrollTop = newScrollTop;
+  });
+}
