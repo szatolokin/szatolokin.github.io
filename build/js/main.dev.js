76d303732a6a0b182ff55ffa73c17a3442b1b380
szatolokin <staszatolokin28@gmail.com>
2020-08-23 20:56:17 +0300

    text moving loop panel

--- build/js/main.dev.js
+++ build/js/main.dev.js
@@ -1,21 +1,3 @@
 "use strict";
 
-$(document).ready(function () {
-  var isActive = false;
-  $(".block").css("user-select", "none");
-  $(".block").text("");
-  $(".block").click(function () {
-    if (isActive) {
-      $(this).css("background-color", "");
-      $("[type=\"checkbox\"]").removeAttr("checked");
-    } else {
-      $(this).css("background-color", "lightgreen");
-      $("[type=\"checkbox\"]").attr("checked", "checked");
-    }
-
-    isActive = !isActive;
-  });
-  $(".block").on("transitionend", function (event) {
-    console.log(event.originalEvent.propertyName);
-  });
-});
+$(document).ready(function () {});
--- main.js
+++ main.js
@@ -1,25 +1,5 @@
 "use strict";
 
 $(document).ready(function () {
-  let
-    isActive = false;
-
-  $(".block").css("user-select", "none");
-  $(".block").text("");
-
-  $(".block").click(function () {
-    if (isActive) {
-      $(this).css("background-color", "");
-      $("[type=\"checkbox\"]").removeAttr("checked");
-    } else {
-      $(this).css("background-color", "lightgreen");
-      $("[type=\"checkbox\"]").attr("checked", "checked");
-    }
-
-    isActive = !isActive;
-  });
-
-  $(".block").on("transitionend", function (event) {
-    console.log(event.originalEvent.propertyName);
-  });
+  
 });
