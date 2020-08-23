e05e4dc912ef000cc83b42e544af33c8da72dfa1
szatolokin <staszatolokin28@gmail.com>
2020-08-23 18:30:02 +0300

    test

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 "use strict";
 
 $(document).ready(function () {
-  alert($(window).width());
+  let
+    isActive = false;
+
+  $(".block").css("user-select", "none");
+  $(".block").text("");
+
+  $(".block").click(function () {
+    if (isActive) {
+      $(this).css("background-color", "");
+      $("[type=\"checkbox\"]").removeAttr("checked");
+    } else {
+      $(this).css("background-color", "lightgreen");
+      $("[type=\"checkbox\"]").attr("checked", "checked");
+    }
+
+    isActive = !isActive;
+  });
+
+  $(".block").on("transitionend", function (event) {
+    console.log(event.originalEvent.propertyName);
+  });
 });
