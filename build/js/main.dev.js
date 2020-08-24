40c6970e7287759b551456eaa4a4b7ddca1b4446
szatolokin <staszatolokin28@gmail.com>
2020-08-24 11:45:25 +0300

    landing v0.0.2

--- build/js/main.dev.js
+++ build/js/main.dev.js
@@ -3,8 +3,33 @@
 $(document).ready(function () {
   slider();
   gallery();
+  menu();
 });
 
 function slider() {}
 
 function gallery() {}
+
+function menu() {
+  var button = ".menu-btn",
+      drop = ".menu-drop",
+      searchBtn = ".menu-drop-search-btn",
+      shadow = ".shadow";
+  var isActive = false;
+  $(button).click(function () {
+    if (isActive) {
+      $(button).addClass("active");
+      $(drop).addClass("active");
+      $(shadow).addClass("active");
+    } else {
+      $(button).removeClass("active");
+      $(drop).removeClass("active");
+      $(shadow).removeClass("active");
+    }
+
+    isActive = !isActive;
+  });
+  $(searchBtn).click(function (e) {
+    e.preventDefault();
+  });
+}
--- main.js
+++ main.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
   slider();
 
   gallery();
+
+  menu();
 });
 
 function slider() {
@@ -12,4 +14,33 @@ function slider() {
 
 function gallery() {
 
+}
+
+function menu() {
+  const
+    button = ".menu-btn",
+    drop = ".menu-drop",
+    searchBtn = ".menu-drop-search-btn",
+    shadow = ".shadow";
+
+  let
+    isActive = false;
+
+  $(button).click(function () {
+    if (isActive) {
+      $(button).addClass("active");
+      $(drop).addClass("active");
+      $(shadow).addClass("active");
+    } else {
+      $(button).removeClass("active");
+      $(drop).removeClass("active");
+      $(shadow).removeClass("active");
+    }
+
+    isActive = !isActive;
+  });
+
+  $(searchBtn).click(function (e) {
+    e.preventDefault();
+  });
 }
