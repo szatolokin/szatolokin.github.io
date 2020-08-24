e3379e17b6af0f2a4efdfb056a0a0a4cd2ff8c73
szatolokin <staszatolokin28@gmail.com>
2020-08-24 19:56:04 +0300

    landing v0.2.0

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 "use strict";
 
 $(document).ready(function () {
-  
+  tabs();
+
+  sticky();
 });
+
+function tabs() {
+  let
+    activeTabNum = 1;
+
+  $(".main-tabs-panel-item").click(function (e) {
+    const
+      clickTabNum = $(this).attr("data-tab"),
+      tabWidth = $(".main-tabs-blocks-wrapper-item").width() + 50;
+
+    if (clickTabNum !== activeTabNum) {
+      $(".main-tabs-panel-item.active").removeClass("active");
+      activeTabNum = clickTabNum;
+      $(`.main-tabs-panel-item[data-tab="${activeTabNum}"]`).addClass("active");
+      console.log(tabWidth);
+      let newTranslateX = -tabWidth * (activeTabNum - 1);
+      $(".main-tabs-blocks-wrapper").css("transform", `translateX(${newTranslateX + "px"})`);
+    }
+  });
+}
+
+function sticky() {
+  const sticky = new Sticky('.main-block2-sticky');
+}
