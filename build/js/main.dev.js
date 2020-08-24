3ad743e0e75aead5239a14f32b3326c128310a91
szatolokin <staszatolokin28@gmail.com>
2020-08-24 09:50:44 +0300

    landing v0.0.1

--- build/js/main.dev.js
+++ build/js/main.dev.js
@@ -5,37 +5,6 @@ $(document).ready(function () {
   gallery();
 });
 
-function slider() {
-  var mySwiper = new Swiper(".swiper-container", {
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    },
-    scrollbar: {
-      el: '.swiper-scrollbar'
-    }
-  });
-}
+function slider() {}
 
-function gallery() {
-  var isActive = false;
-  $(".block").click(function () {
-    if (!isActive) {
-      $(".gallery").fancybox({});
-      isActive = true;
-      $(this).css("background-color", "lightgreen");
-    } else {
-      isActive = false;
-      $(this).css("background-color", "");
-    }
-  });
-  $(".gallery").click(function (event) {
-    if (!isActive) {
-      event.preventDefault();
-    }
-  });
-}
+function gallery() {}
--- main.js
+++ main.js
@@ -7,44 +7,9 @@ $(document).ready(function () {
 });
 
 function slider() {
-  const
-    mySwiper = new Swiper(".swiper-container", {
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
 
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      },
-    });
 }
 
 function gallery() {
-  let
-    isActive = false;
 
-  $(".block").click(function () {
-    if (!isActive) {
-      $(".gallery").fancybox({});
-      isActive = true;
-
-      $(this).css("background-color", "lightgreen");
-    } else {
-      isActive = false;
-
-      $(this).css("background-color", "");
-    }
-  });
-
-  $(".gallery").click(function (event) {
-    if (!isActive) {
-      event.preventDefault();
-    }
-  });
 }
