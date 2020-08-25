c4a02f0c9474190f170bf038c637315899e99737
szatolokin <staszatolokin28@gmail.com>
2020-08-25 04:07:13 +0300

    landing v0.3.1

--- build/js/main.dev.js
+++ build/js/main.dev.js
@@ -1,10 +1,40 @@
 "use strict";
 
-$(document).ready(function () {
-  sticker();
-});
+window.onload = function () {
+  sticker(".sticker", ".cards", ".tabs");
+};
 
-function sticker() {
-  var stickerClassName = "sticker",
-      sticker = $("." + stickerClassName);
+function sticker(stickerSelector, startSelector, endSelector) {
+  var sticker = $(stickerSelector),
+      pos = sticker.position().top,
+      height = sticker.height(),
+      start = $(startSelector),
+      end = $(endSelector);
+  var stickyNow = false;
+  $(window).scroll(function () {
+    var startY = start.offset().top,
+        endY = end.offset().top - pos * 2 - height,
+        scrollY = $(window).scrollTop();
+
+    if (scrollY > startY && scrollY < endY) {
+      sticker.offset({
+        top: scrollY + pos
+      });
+      sticker.removeClass("non-active");
+      stickyNow = true;
+    } else if (stickyNow) {
+      if (scrollY <= startY) {
+        sticker.offset({
+          top: startY + pos
+        });
+      } else {
+        sticker.offset({
+          top: endY + pos
+        });
+        sticker.addClass("non-active");
+      }
+
+      stickyNow = false;
+    }
+  });
 }
--- main.js
+++ main.js
@@ -1,11 +1,42 @@
 "use strict";
 
-$(document).ready(function () {
-  sticker();
-});
+window.onload = function () {
+  sticker(".sticker", ".cards", ".tabs");
+};
 
-function sticker() {
+function sticker(stickerSelector, startSelector, endSelector) {
   const
-    stickerClassName = "sticker",
-    sticker = $("." + stickerClassName);
+    sticker = $(stickerSelector),
+    pos = sticker.position().top,
+    height = sticker.height(),
+    start = $(startSelector),
+    end = $(endSelector);
+
+  let
+    stickyNow = false;
+
+  $(window).scroll(function () {
+    const
+      startY = start.offset().top,
+      endY = end.offset().top - (pos * 2) - height,
+      scrollY = $(window).scrollTop();
+
+    if (scrollY > startY && scrollY < endY) {
+      sticker.offset({ top: scrollY + pos });
+      
+      sticker.removeClass("non-active");
+
+      stickyNow = true;
+    } else if (stickyNow) {
+      if (scrollY <= startY) {
+        sticker.offset({ top: startY + pos });
+      } else {
+        sticker.offset({ top: endY + pos });
+
+        sticker.addClass("non-active");
+      }
+
+      stickyNow = false;
+    }
+  });
 }
