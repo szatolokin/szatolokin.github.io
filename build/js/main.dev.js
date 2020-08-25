f1d534b51281530d160c4ddb9cb559ba6477ca0e
szatolokin <staszatolokin28@gmail.com>
2020-08-25 03:00:07 +0300

    landing v0.3.0

--- build/js/main.dev.js
+++ build/js/main.dev.js
@@ -1,27 +1,10 @@
 "use strict";
 
 $(document).ready(function () {
-  tabs();
-  sticky();
+  sticker();
 });
 
-function tabs() {
-  var activeTabNum = 1;
-  $(".main-tabs-panel-item").click(function (e) {
-    var clickTabNum = $(this).attr("data-tab"),
-        tabWidth = $(".main-tabs-blocks-wrapper-item").width() + 50;
-
-    if (clickTabNum !== activeTabNum) {
-      $(".main-tabs-panel-item.active").removeClass("active");
-      activeTabNum = clickTabNum;
-      $(".main-tabs-panel-item[data-tab=\"".concat(activeTabNum, "\"]")).addClass("active");
-      console.log(tabWidth);
-      var newTranslateX = -tabWidth * (activeTabNum - 1);
-      $(".main-tabs-blocks-wrapper").css("transform", "translateX(".concat(newTranslateX + "px", ")"));
-    }
-  });
-}
-
-function sticky() {
-  var sticky = new Sticky('.main-block2-sticky');
+function sticker() {
+  var stickerClassName = "sticker",
+      sticker = $("." + stickerClassName);
 }
--- main.js
+++ main.js
@@ -1,31 +1,11 @@
 "use strict";
 
 $(document).ready(function () {
-  tabs();
-
-  sticky();
+  sticker();
 });
 
-function tabs() {
-  let
-    activeTabNum = 1;
-
-  $(".main-tabs-panel-item").click(function (e) {
-    const
-      clickTabNum = $(this).attr("data-tab"),
-      tabWidth = $(".main-tabs-blocks-wrapper-item").width() + 50;
-
-    if (clickTabNum !== activeTabNum) {
-      $(".main-tabs-panel-item.active").removeClass("active");
-      activeTabNum = clickTabNum;
-      $(`.main-tabs-panel-item[data-tab="${activeTabNum}"]`).addClass("active");
-      console.log(tabWidth);
-      let newTranslateX = -tabWidth * (activeTabNum - 1);
-      $(".main-tabs-blocks-wrapper").css("transform", `translateX(${newTranslateX + "px"})`);
-    }
-  });
-}
-
-function sticky() {
-  const sticky = new Sticky('.main-block2-sticky');
+function sticker() {
+  const
+    stickerClassName = "sticker",
+    sticker = $("." + stickerClassName);
 }
